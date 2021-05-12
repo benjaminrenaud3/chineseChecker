67cdfa7cfcd8fe0af600bb7b24a0379ef43b6e5e
Quentin <quentin1.thomas@epitech.eu>
2021-05-12 18:48:52 +0200

    dot placement and color

--- chinesechecker/front/src/App.tsx
+++ chinesechecker/front/src/App.tsx
@@ -4,130 +4,152 @@ import Circle from "./Circle";
 const r = Math.sqrt(3);
 const scale = 1;
 
+interface Dot {
+  x: number;
+  y: number;
+  color: string;
+}
+const spots: Dot[] = [
+  //bottom
+  { x: 0, y: 8, color: "red" },
+  { x: -1, y: 7, color: "red" },
+  { x: 1, y: 7, color: "red" },
+  { x: -2, y: 6, color: "red" },
+  { x: 0, y: 6, color: "red" },
+  { x: 2, y: 6, color: "red" },
+  { x: -3, y: 5, color: "red" },
+  { x: -1, y: 5, color: "red" },
+  { x: 1, y: 5, color: "red" },
+  { x: 3, y: 5, color: "red" },
+  // left bot corner
+  { x: -12, y: 4, color: "green" },
+  { x: -10, y: 4, color: "green" },
+  { x: -8, y: 4, color: "green" },
+  { x: -6, y: 4, color: "green" },
+  { x: -11, y: 3, color: "green" },
+  { x: -7, y: 3, color: "green" },
+  { x: -10, y: 2, color: "green" },
+  { x: -8, y: 2, color: "green" },
+  { x: -9, y: 3, color: "green" },
+  { x: -9, y: 1, color: "green" },
+
+  //middle
+  { x: -7, y: 1, color: "white" },
+  { x: -5, y: 3, color: "white" },
+  { x: -4, y: 4, color: "white" },
+  { x: -6, y: 2, color: "white" },
+  { x: -2, y: 4, color: "white" },
+  { x: -4, y: 2, color: "white" },
+  { x: -2, y: 2, color: "white" },
+  { x: 0, y: 2, color: "white" },
+  { x: 0, y: 4, color: "white" },
+  { x: 2, y: 4, color: "white" },
+  { x: 4, y: 4, color: "white" },
+  { x: -3, y: 3, color: "white" },
+  { x: -1, y: 3, color: "white" },
+  { x: 1, y: 3, color: "white" },
+  { x: 3, y: 3, color: "white" },
+  { x: 5, y: 3, color: "white" },
+  { x: 2, y: 2, color: "white" },
+  { x: 4, y: 2, color: "white" },
+  { x: 6, y: 2, color: "white" },
+  { x: -5, y: 1, color: "white" },
+  { x: -3, y: 1, color: "white" },
+  { x: -1, y: 1, color: "white" },
+  { x: 1, y: 1, color: "white" },
+  { x: 3, y: 1, color: "white" },
+  { x: 5, y: 1, color: "white" },
+  { x: 7, y: 1, color: "white" },
+  { x: -8, y: 0, color: "white" },
+  { x: -6, y: 0, color: "white" },
+  { x: -4, y: 0, color: "white" },
+  { x: -2, y: 0, color: "white" },
+  { x: 0, y: 0, color: "white" },
+  { x: 2, y: 0, color: "white" },
+  { x: 4, y: 0, color: "white" },
+  { x: 6, y: 0, color: "white" },
+  { x: 8, y: 0, color: "white" },
+  { x: -7, y: -1, color: "white" },
+  { x: -5, y: -1, color: "white" },
+  { x: -3, y: -1, color: "white" },
+  { x: -1, y: -1, color: "white" },
+  { x: 1, y: -1, color: "white" },
+  { x: 3, y: -1, color: "white" },
+  { x: 5, y: -1, color: "white" },
+  { x: 7, y: -1, color: "white" },
+  { x: -6, y: -2, color: "white" },
+  { x: -4, y: -2, color: "white" },
+  { x: -2, y: -2, color: "white" },
+  { x: 0, y: -2, color: "white" },
+  { x: 2, y: -2, color: "white" },
+  { x: 4, y: -2, color: "white" },
+  { x: 6, y: -2, color: "white" },
+  { x: -5, y: -3, color: "white" },
+  { x: -3, y: -3, color: "white" },
+  { x: -1, y: -3, color: "white" },
+  { x: 1, y: -3, color: "white" },
+  { x: 3, y: -3, color: "white" },
+  { x: 5, y: -3, color: "white" },
+
+  { x: -4, y: -4, color: "white" },
+  { x: -2, y: -4, color: "white" },
+  { x: 0, y: -4, color: "white" },
+  { x: 2, y: -4, color: "white" },
+
+  { x: 4, y: -4, color: "white" },
+  //right bottom corner
+  { x: 6, y: 4, color: "blue" },
+  { x: 8, y: 4, color: "blue" },
+  { x: 10, y: 4, color: "blue" },
+  { x: 12, y: 4, color: "blue" },
+  { x: 7, y: 3, color: "blue" },
+  { x: 9, y: 3, color: "blue" },
+  { x: 11, y: 3, color: "blue" },
+  { x: 9, y: 1, color: "blue" },
+  { x: 8, y: 2, color: "blue" },
+  { x: 10, y: 2, color: "blue" },
+
+//top right corner
+  { x: 9, y: -1, color: "orange" },
+  { x: 8, y: -2, color: "orange" },
+  { x: 10, y: -2, color: "orange" },
+  { x: 6, y: -4, color: "orange" },
+  { x: 8, y: -4, color: "orange" },
+  { x: 10, y: -4, color: "orange" },
+  { x: 12, y: -4, color: "orange" },
+  { x: 7, y: -3, color: "orange" },
+  { x: 9, y: -3, color: "orange" },
+  { x: 11, y: -3, color: "orange" },
+
+  //top left corner
+  { x: -10, y: -2, color: "brown" },
+  { x: -8, y: -2, color: "brown" },
+  { x: -9, y: -1, color: "brown" },
+  { x: -11, y: -3, color: "brown" },
+  { x: -9, y: -3, color: "brown" },
+  { x: -7, y: -3, color: "brown" },
+  { x: -12, y: -4, color: "brown" },
+  { x: -10, y: -4, color: "brown" },
+  { x: -8, y: -4, color: "brown" },
+  { x: -6, y: -4, color: "brown" },
+  { x: -6, y: -4, color: "brown" },
+  
 
 
-const spots = [
-  [0, 8],
-  [-1, 7],
-  [1, 7],
-  [-2, 6],
-  [0, 6],
-  [2, 6],
-  [-3, 5],
-  [-1, 5],
-  [1, 5],
-  [3, 5],
-  [-12, 4],
-  [-10, 4],
-  [-8, 4],
-  [-6, 4],
-  [-4, 4],
-  [-2, 4],
-  [0, 4],
-  [2, 4],
-  [4, 4],
-  [6, 4],
-  [8, 4],
-  [10, 4],
-  [12, 4],
-  [-11, 3],
-  [-9, 3],
-  [-7, 3],
-  [-5, 3],
-  [-3, 3],
-  [-1, 3],
-  [1, 3],
-  [3, 3],
-  [5, 3],
-  [7, 3],
-  [9, 3],
-  [11, 3],
-  [-10, 2],
-  [-8, 2],
-  [-6, 2],
-  [-4, 2],
-  [-2, 2],
-  [0, 2],
-  [2, 2],
-  [4, 2],
-  [6, 2],
-  [8, 2],
-  [10, 2],
-  [-9, 1],
-  [-7, 1],
-  [-5, 1],
-  [-3, 1],
-  [-1, 1],
-  [1, 1],
-  [3, 1],
-  [5, 1],
-  [7, 1],
-  [9, 1],
-  [-8, 0],
-  [-6, 0],
-  [-4, 0],
-  [-2, 0],
-  [0, 0],
-  [2, 0],
-  [4, 0],
-  [6, 0],
-  [8, 0],
-  [-9, -1],
-  [-7, -1],
-  [-5, -1],
-  [-3, -1],
-  [-1, -1],
-  [1, -1],
-  [3, -1],
-  [5, -1],
-  [7, -1],
-  [9, -1],
-  [-10, -2],
-  [-8, -2],
-  [-6, -2],
-  [-4, -2],
-  [-2, -2],
-  [0, -2],
-  [2, -2],
-  [4, -2],
-  [6, -2],
-  [8, -2],
-  [10, -2],
-  [-11, -3],
-  [-9, -3],
-  [-7, -3],
-  [-5, -3],
-  [-3, -3],
-  [-1, -3],
-  [1, -3],
-  [3, -3],
-  [5, -3],
-  [7, -3],
-  [9, -3],
-  [11, -3],
-  [-12, -4],
-  [-10, -4],
-  [-8, -4],
-  [-6, -4],
-  [-4, -4],
-  [-2, -4],
-  [0, -4],
-  [2, -4],
-  [4, -4],
-  [6, -4],
-  [8, -4],
-  [10, -4],
-  [12, -4],
-  [0, -8],
-  [-1, -7],
-  [1, -7],
-  [-2, -6],
-  [0, -6],
-  [2, -6],
-  [-3, -5],
-  [-1, -5],
-  [1, -5],
-  [3, -5],
+
+
+
+  //top 
+  { x: 0, y: -8, color: "pink" },
+  { x: -1, y: -7, color: "pink" },
+  { x: 1, y: -7, color: "pink" },
+  { x: -2, y: -6, color: "pink" },
+  { x: 0, y: -6, color: "pink" },
+  { x: 2, y: -6, color: "pink" },
+  { x: -3, y: -5, color: "pink" },
+  { x: -1, y: -5, color: "pink" },
+  { x: 1, y: -5, color: "pink" },
+  { x: 3, y: -5, color: "pink" },
 ];
 
 const outier = [
@@ -157,10 +179,6 @@ const App = () => (
       margin: 0,
       padding: 0,
     }}
-    onContextMenu={(e) => {
-      e.preventDefault();
-      console.log("haha");
-    }}
   >
     <svg viewBox="0 0 32 36" style={{ background: "#000" }}>
       <polygon
@@ -170,9 +188,17 @@ const App = () => (
         fill="transparent"
         strokeLinejoin="round"
       />
-      {spots.map((points, index) => (
-        <Circle key={index} x={points[0]} y={points[1]} scale={scale} />
-      ))}
+      {spots.map((points, index) => {
+        return (
+          <Circle
+            key={index}
+            x={points.x}
+            y={points.y}
+            scale={scale}
+            color={points.color}
+          />
+        );
+      })}
     </svg>
   </div>
 );
--- chinesechecker/front/src/Circle.tsx
+++ chinesechecker/front/src/Circle.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 const r = Math.sqrt(3)
 
-const Circle = ({scale = 1, x, y}) => 
+interface Props {
+  scale: number;
+  x: number;
+  y : number;
+  color: string;
+}
+const Circle = ({scale = 1, x, y, color} : Props) => 
   <circle 
     cx={`calc(16 + ${x*scale})`} 
     cy={`calc(18 + ${y*r*scale})`} 
     r={`${.8 * scale}`}
-    fill='white'
+    fill={color}
   />
 
 export default Circle
